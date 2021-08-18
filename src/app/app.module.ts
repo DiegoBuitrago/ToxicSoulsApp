89fcc3c069756272170d505b9d1cb84944780a21
EdisonArias7 <edison.arias02@uptc.edu.co>
2021-08-18 14:28:21 -0500

    Creacion de componente y/o modulo Autenticacion de usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthUuserComponent } from './auth-uuser/auth-uuser.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthUuserComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/auth-uuser/auth-uuser.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-uuser',
+  templateUrl: './auth-uuser.component.html',
+  styleUrls: ['./auth-uuser.component.css']
+})
+export class AuthUuserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
