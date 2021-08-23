641c14b35c4d7e07178d2436b00c8dec5b9be665
EdisonArias7 <edison.arias02@uptc.edu.co>
2021-08-22 23:20:53 -0500

    Pagina principal creada en componente homepage desarrollada con las secciones y elementos, sin lateral

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Router, RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthUserComponent } from './auth-user/auth-user.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
+const appRoutes: Routes = [
+  {path:'login',component:AuthUserComponent},
+  {path:'homepage',component:HomePageComponent},
+  {path: '',
+    redirectTo:'/homepage',
+    pathMatch:'full'
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthUserComponent
+    AuthUserComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
